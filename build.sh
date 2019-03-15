a25d32d0ade387ddb78200dc2555547217e67461
rickone <rickone@sina.com>
2019-03-15 11:07:34 +0800

    feat: build.sh
    Change-Id: Ib33becdcd1650498adf419e251644d061b01dddf

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-CPKG=`which cpkg`
-
-if [ "$CPKG" == "" ]; then
-	mkdir -p pkg
-	if [ ! -d "pkg/cpkg" ]; then
-		git clone https://github.com/rickone/cpkg.git pkg/cpkg
-	fi
-	CPKG_PATH=`pwd`/pkg/cpkg pkg/cpkg/cpkg build
-else
-	cpkg build
+#mkdir -p pkg
+if [ ! -d "pkg/cpkg" ]; then
+	git clone https://github.com/rickone/cpkg.git pkg/cpkg
 fi
+pkg/cpkg/cpkg build
